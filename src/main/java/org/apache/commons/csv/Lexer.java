c64670fbfbab007821763e3730c0068636c72842
Yuzhan Jiang <36880517+DarrenJAN@users.noreply.github.com>
2024-11-05 13:54:23 -0500

    Revert "Add support in Commons CSV for tracking byte positions during parsing…"
    This reverts commit c413aac952e779268d2b3418015a89e433c3c703.

--- src/main/java/org/apache/commons/csv/Lexer.java
+++ src/main/java/org/apache/commons/csv/Lexer.java
@@ -104,15 +104,6 @@ final class Lexer implements Closeable {
     }
 
     /**
-     * Returns the number of bytes read
-     *
-     * @return the number of bytes read
-     */
-    long getBytesRead() {
-        return reader.getBytesRead();
-    }
-
-    /**
      * Returns the current line number
      *
      * @return the current line number
